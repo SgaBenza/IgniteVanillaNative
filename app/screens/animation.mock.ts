20c30a95c2a70633cafb8ed2948e236a1a4e171f
Ivan Rossi <ivanross.me@gmail.com>
2021-11-03 15:33:42 +0100

    move animation logic to animation.mock.ts

---
+++ app/screens/animation.mock.ts
@@ -0,0 +1,97 @@
+import { last, times } from "lodash"
+import { Dimensions } from "react-native"
+import SimplexNoise from "simplex-noise"
+
+const { width } = Dimensions.get("window")
+
+const simplex = new SimplexNoise()
+
+const vizWidth = width
+const heightWidth = 200
+
+const buildShape = (x: number, shapeWidth: number) => {
+  return (Math.sin(2 * Math.PI * (x / shapeWidth) + Math.PI / 2) * 0.5 + 0.5) * heightWidth
+}
+
+const pointsAmount = 100
+const pointStep = vizWidth / pointsAmount
+
+const generatePoints = (shapeWidth: number, getY: (p: { x: number; y: number }) => number) =>
+  times(shapeWidth / pointStep)
+    .map((x) => x * pointStep)
+    .map((x) => {
+      return { x, y: getY({ x, y: buildShape(x, shapeWidth) }) }
+    })
+
+const track = generatePoints(width, (p) => p.y)
+
+const repetitions = [
+  [
+    generatePoints(width, (p) => p.y + simplex.noise2D(p.x * 0.004 + 100, p.x * 0.004) * 30),
+    generatePoints(width, (p) => p.y + simplex.noise2D(p.x * 0.001, p.x * 0.004) * 30),
+  ],
+  [
+    generatePoints(width * 1.5, (p) => p.y + simplex.noise2D(p.x * 0.004 + 100, p.x * 0.004) * 30),
+    generatePoints(width * 1.5, (p) => p.y + simplex.noise2D(p.x * 0.001, p.x * 0.004) * 30),
+  ],
+  [
+    generatePoints(width * 0.8, (p) => p.y + simplex.noise2D(p.x * 0.001 + 100, p.x * 0.004) * 30),
+    generatePoints(width * 0.8, (p) => p.y + simplex.noise2D(p.x * 0.001, p.x * 0.004) * 30),
+  ],
+  [
+    generatePoints(width * 1.8, (p) => p.y + simplex.noise2D(p.x * 0.001 + 100, p.x * 0.004) * 30),
+    generatePoints(width * 1.8, (p) => p.y + simplex.noise2D(p.x * 0.001, p.x * 0.004) * 30),
+  ],
+]
+
+export const color = {
+  left: "#00F0FF",
+  right: "#FF97F0",
+}
+
+interface AnimationState {
+  repetitionIndex: number
+  pointIndex: number
+}
+
+export const MockAnimation = {
+  initialState: (): AnimationState => ({ pointIndex: 0, repetitionIndex: 0 }),
+
+  computeFrameData: (state: AnimationState) => {
+    const [left, right] = repetitions[state.repetitionIndex]
+
+    const leftPoint = left[state.pointIndex] ?? last(left)
+    const rightPoint = right[state.pointIndex] ?? last(right)
+    const trackPoint = track[state.pointIndex] ?? last(track)
+
+    const translateX = Math.max(0, state.pointIndex - track.length) * pointStep
+
+    const rightVisiblePoints = right.slice(0, state.pointIndex)
+    const leftVisiblePoints = left.slice(0, state.pointIndex)
+
+    return {
+      leftPoint,
+      rightPoint,
+      trackPoint,
+      translateX,
+      rightVisiblePoints,
+      leftVisiblePoints,
+      track,
+    }
+  },
+
+  updateAnimationState: (state: AnimationState): AnimationState => {
+    const newState = { ...state }
+
+    const [left, right] = repetitions[state.repetitionIndex]
+
+    if (newState.pointIndex >= left.length && newState.pointIndex >= right.length) {
+      newState.pointIndex = 0
+      newState.repetitionIndex = (newState.repetitionIndex + 1) % repetitions.length
+    } else {
+      newState.pointIndex++
+    }
+
+    return newState
+  },
+}
--- app/screens/gcanvas/gcanvas.tsx
+++ app/screens/gcanvas/gcanvas.tsx
@@ -1,49 +1,11 @@
 import React from "react"
-import { random, times, last } from "lodash-es"
-import { View, Text, StyleSheet, Dimensions, AppState } from "react-native"
+import { View, StyleSheet, Dimensions } from "react-native"
 import { GCanvasView } from "@flyskywhy/react-native-gcanvas"
-import { TouchableOpacity } from "react-native-gesture-handler"
-import SimplexNoise from "simplex-noise"
 import * as Canova from "canova"
 
 import { curveMonotoneX, line } from "d3-shape"
-const { height, width } = Dimensions.get("window")
-
-const simplex = new SimplexNoise()
-
-const vizWidth = width
-const heightWidth = 200
-
-const buildShape = (x: number, shapeWidth: number) => {
-  return (Math.sin(2 * Math.PI * (x / shapeWidth) + Math.PI / 2) * 0.5 + 0.5) * heightWidth
-}
-
-const pointsAmount = 100
-const pointStep = vizWidth / pointsAmount
-
-const generatePoints = (shapeWidth: number, getY: (p: { x: number; y: number }) => number) =>
-  times(shapeWidth / pointStep)
-    .map((x) => x * pointStep)
-    .map((x) => {
-      return { x, y: getY({ x, y: buildShape(x, shapeWidth) }) }
-    })
-
-const track = generatePoints(width, (p) => p.y)
-
-const repetitions = [
-  [
-    generatePoints(width, (p) => p.y + simplex.noise2D(p.x * 0.004 + 100, p.x * 0.004) * 30),
-    generatePoints(width, (p) => p.y + simplex.noise2D(p.x * 0.001, p.x * 0.004) * 30),
-  ],
-  [
-    generatePoints(width * 1.5, (p) => p.y + simplex.noise2D(p.x * 0.004 + 100, p.x * 0.004) * 30),
-    generatePoints(width * 1.5, (p) => p.y + simplex.noise2D(p.x * 0.001, p.x * 0.004) * 30),
-  ],
-  [
-    generatePoints(width * 0.8, (p) => p.y + simplex.noise2D(p.x * 0.001 + 100, p.x * 0.004) * 30),
-    generatePoints(width * 0.8, (p) => p.y + simplex.noise2D(p.x * 0.001, p.x * 0.004) * 30),
-  ],
-]
+import { color, MockAnimation } from "../animation.mock"
+const { width } = Dimensions.get("window")
 
 const pathLine = line<{ x: number; y: number }>()
   .x((d) => d.x)
@@ -64,7 +26,7 @@ export const GCanvasScreen = () => {
     ctxRef.current = canvasRef.current.getContext("2d")
   }
 
-  const animationState = React.useRef({ pointIndex: 0, repetitionIndex: 0 }).current
+  const animationState = React.useRef(MockAnimation.initialState()).current
 
   const draw = () => {
     // On Android, sometimes this.isGReactTextureViewReady is false e.g.
@@ -79,13 +41,15 @@ export const GCanvasScreen = () => {
 
     // if (ctx && AppState.currentState === "active") {
     if (ctx) {
-      const [left, right] = repetitions[animationState.repetitionIndex]
-
-      const leftPoint = left[animationState.pointIndex] ?? last(left)
-      const rightPoint = right[animationState.pointIndex] ?? last(right)
-      const trackPoint = track[animationState.pointIndex] ?? last(track)
-
-      const translateX = Math.max(0, animationState.pointIndex - track.length) * pointStep
+      const {
+        leftPoint,
+        rightPoint,
+        trackPoint,
+        leftVisiblePoints,
+        rightVisiblePoints,
+        translateX,
+        track,
+      } = MockAnimation.computeFrameData(animationState)
 
       ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
       Canova.draw(
@@ -104,44 +68,39 @@ export const GCanvasScreen = () => {
           (ctx) => {
             ctx.beginPath()
 
-            ctx.strokeStyle = "#00F0FF"
+            ctx.strokeStyle = color.right
             ctx.lineWidth = 4
             pathLine.context(ctx)
-            pathLine(right.slice(0, animationState.pointIndex))
+            pathLine(rightVisiblePoints)
             ctx.stroke()
           },
 
           (ctx) => {
             ctx.beginPath()
 
-            ctx.strokeStyle = "#FF97F0"
+            ctx.strokeStyle = color.left
             ctx.lineWidth = 4
             pathLine.context(ctx)
-            pathLine(left.slice(0, animationState.pointIndex))
+            pathLine(leftVisiblePoints)
             ctx.stroke()
           },
           Canova.circle(trackPoint.x, trackPoint.y, 6, {
             fill: "white",
           }),
           Canova.circle(rightPoint.x, rightPoint.y, 6, {
-            stroke: "#00F0FF",
+            stroke: color.right,
             strokeWidth: 4,
             fill: "black",
           }),
           Canova.circle(leftPoint.x, leftPoint.y, 6, {
-            stroke: "#FF97F0",
+            stroke: color.left,
             strokeWidth: 4,
             fill: "black",
           }),
         ]),
       )
 
-      if (animationState.pointIndex >= left.length && animationState.pointIndex >= right.length) {
-        animationState.pointIndex = 0
-        animationState.repetitionIndex = (animationState.repetitionIndex + 1) % repetitions.length
-      } else {
-        animationState.pointIndex++
-      }
+      Object.assign(animationState, MockAnimation.updateAnimationState(animationState))
     }
   }
 
