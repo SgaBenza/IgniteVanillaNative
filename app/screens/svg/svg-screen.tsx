85cf1a65328a2bd7d7df9d7c661f38da0fe0852d
Ivan Rossi <ivanross.me@gmail.com>
2021-11-04 10:47:08 +0100

    add video background

--- app/screens/svg/svg-screen.tsx
+++ app/screens/svg/svg-screen.tsx
@@ -3,10 +3,13 @@ import { View, SafeAreaView, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { observer } from "mobx-react-lite"
 import Svg, { Circle, Path, G } from "react-native-svg"
+import Video from "react-native-video"
 import { NavigatorParamList } from "../../navigators"
 import { line, curveMonotoneX } from "d3-shape"
 import { color, MockAnimation, vizHeight, vizWidth } from "../animation.mock"
 import { useIsFocused } from "@react-navigation/core"
+
+console.log("video: ", require("../bg-video.mov"))
 
 const SVG_STYLE: ViewStyle = {
   width: vizWidth,
@@ -14,6 +17,34 @@ const SVG_STYLE: ViewStyle = {
 }
 
 const WRAPPER_STYLE: ViewStyle = { height: "100%", justifyContent: "center" }
+
+const VIDEO_STYLE: ViewStyle = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+}
+export const SvgScreen: React.FC<StackScreenProps<NavigatorParamList, "svg">> = observer(() => {
+  return (
+    <View>
+      <Video
+        source={require("../bg-video.mov")}
+        style={VIDEO_STYLE as any}
+        controls={false}
+        playInBackground
+        muted
+        repeat
+        disableFocus
+        resizeMode="cover"
+      />
+
+      <View style={WRAPPER_STYLE}>
+        <SvgAnimation />
+      </View>
+    </View>
+  )
+})
 
 const pathLine = line<{ x: number; y: number }>()
   .x((d) => d.x)
@@ -29,7 +60,7 @@ function useConst<T>(initial: T): T {
   return React.useRef(initial).current
 }
 
-export const SvgScreen: React.FC<StackScreenProps<NavigatorParamList, "svg">> = observer(() => {
+const SvgAnimation = () => {
   const isFocused = useIsFocused()
   const render = useRender()
   const animationState = useConst(MockAnimation.initialState())
@@ -67,34 +98,30 @@ export const SvgScreen: React.FC<StackScreenProps<NavigatorParamList, "svg">> = 
   }, [isFocused])
 
   return (
-    <SafeAreaView>
-      <View style={WRAPPER_STYLE}>
-        <Svg viewBox={`0 0 ${vizWidth} ${vizHeight}`} style={SVG_STYLE}>
-          <G translateX={-translateX} translateY={40}>
-            <Path d={pathLine(track)} stroke={color.track} strokeWidth={20} />
-            <Path d={pathLine(leftVisiblePoints)} stroke={color.left} strokeWidth={4} />
-            <Path d={pathLine(rightVisiblePoints)} stroke={color.right} strokeWidth={4} />
+    <Svg viewBox={`0 0 ${vizWidth} ${vizHeight}`} style={SVG_STYLE}>
+      <G translateX={-translateX} translateY={40}>
+        <Path d={pathLine(track)} stroke={color.track} strokeWidth={20} opacity={0.8} />
+        <Path d={pathLine(leftVisiblePoints)} stroke={color.left} strokeWidth={4} />
+        <Path d={pathLine(rightVisiblePoints)} stroke={color.right} strokeWidth={4} />
 
-            <Circle cx={trackPoint.x} cy={trackPoint.y} r={6} fill={"white"} />
-            <Circle
-              cx={leftPoint.x}
-              cy={leftPoint.y}
-              r={6}
-              fill={"black"}
-              stroke={color.left}
-              strokeWidth={4}
-            />
-            <Circle
-              cx={rightPoint.x}
-              cy={rightPoint.y}
-              r={6}
-              fill={"black"}
-              stroke={color.right}
-              strokeWidth={4}
-            />
-          </G>
-        </Svg>
-      </View>
-    </SafeAreaView>
+        <Circle cx={trackPoint.x} cy={trackPoint.y} r={6} fill={"white"} />
+        <Circle
+          cx={leftPoint.x}
+          cy={leftPoint.y}
+          r={6}
+          fill={"black"}
+          stroke={color.left}
+          strokeWidth={4}
+        />
+        <Circle
+          cx={rightPoint.x}
+          cy={rightPoint.y}
+          r={6}
+          fill={"black"}
+          stroke={color.right}
+          strokeWidth={4}
+        />
+      </G>
+    </Svg>
   )
-})
+}
